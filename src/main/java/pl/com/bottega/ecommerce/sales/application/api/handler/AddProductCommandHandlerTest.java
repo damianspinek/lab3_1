3798ae84b2eaaefeb4fd08763eb64242acee788e
Damian Spinek <damian00737@gmail.com>
2015-04-14 18:45:08 +0200

    Dodanie testu zachowania dla metody handle z klasy AddProductCommandHandler

--- src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -1,14 +1,22 @@
 package pl.com.bottega.ecommerce.sales.application.api.handler;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
+import pl.com.bottega.ecommerce.sales.application.api.command.AddProductCommand;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class AddProductCommandHandlerTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addProductCommandHandler_handleTest() {
+		AddProductCommand addProductCommand = new AddProductCommand(Id.generate(), Id.generate(), 1);
+		AddProductCommandHandler addProductCommandHandler= mock(AddProductCommandHandler.class);
+		addProductCommandHandler.handle(addProductCommand);
+		Mockito.verify(addProductCommandHandler, Mockito.times(1)).handle((AddProductCommand)Mockito.any());
 	}
 
 }
