35c51327fa1e19d713d12d3c5715e5584107e795
Damian Spinek <damian00737@gmail.com>
2015-04-14 18:25:10 +0200

    Dodanie testu stanu dla metody issuance z klasy BookKeeper
    Zadaniem tego testu jest sprawdzenie czy nie dodajac do faktury zadnej pozycji otrzymamy pusta fakture

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -61,5 +61,25 @@ public class BookKeeperTest {
 		Mockito.verify(taxPolicy, Mockito.times(2)).calculateTax((ProductType)Mockito.any(), (Money)Mockito.any());
 		
 	}
+	
+	@Test
+	public void bookKeeper_issuanceTest_giveZeroPosition_ExpectedZeroPosition() {
+		ClientData client = new ClientData(Id.generate(), "Spinek");
+		BookKeeper bookKeeper = new BookKeeper( new InvoiceFactory());
+		Money money = new Money(1);
+		ProductData productData =  new ProductData(Id.generate(), money, "Chleb", ProductType.FOOD, new Date());
+		RequestItem requestItem = new RequestItem(productData, 1, money);
+		
+		InvoiceFactory invoiceFactory = mock(InvoiceFactory.class);
+		when(invoiceFactory.create((ClientData)Mockito.any())).thenReturn(new Invoice(Id.generate(), client));
+		TaxPolicy taxPolicy = mock(TaxPolicy.class);
+		when(taxPolicy.calculateTax((ProductType)Mockito.any(), (Money)Mockito.any())).thenReturn(new Tax(money, ""));
+		
+		InvoiceRequest invoiceRequest = new InvoiceRequest(client);
+		
+		Invoice invoice = bookKeeper.issuance(invoiceRequest, taxPolicy);
+		
+		assertThat(invoice.getItems().size(), is(0));		
+	}
 
 }
